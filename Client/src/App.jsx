a925c799dbdc6a78280ff97f953be425a5668b35
Muksit86 <amuksit7@gmail.com>
2026-01-07 21:45:18 +0530

    analytics added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Sidebar from "./Component/Sidebar";
+import { AnalyticsTracker } from "./AnalyticsTracker";
 
 // flex flex-col-reverse md:flex-row h-screen w-full overflow-hidden
 // flex-1 flex flex-col overflow-hidden
@@ -10,6 +11,8 @@ function App() {
       <div className="flex flex-col-reverse md:flex-row h-screen w-full">
         <Sidebar />
         <Outlet />
+
+        <AnalyticsTracker />
       </div>
     </>
   );
